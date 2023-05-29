3bd703ab81a04b5eb3e2858e27c3ab1687a1a0a4
idogwu chinonso <idogwuchinonso1@gmail.com>
2023-05-29 11:14:03 +0100

    started assignment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use core::num;
-
 fn main() {
     println!("Hello, world!");
+    fizz_buzz(4);
 }
 
-fn fizz_buzz(number: u128) {}
+fn fizz_buzz(number: u128) {
+    if number > 5 {
+        println!("above");
+    } else {
+        println!("below");
+    }
+}
